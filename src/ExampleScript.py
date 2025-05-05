17e2a8e0700ea624c55c32b2e8f4025b14192911
Dan Curran <codetown@gmail.com>
2025-05-05 14:00:56 -0400

    Add environment support.

--- src/ExampleScript.py
+++ src/ExampleScript.py
@@ -1,2 +1,14 @@
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+debug_mode = os.getenv("TEST_MODE", False)
+output_dir = os.getenv("OUTPUT_DIR", "/myoutput")
+port = os.getenv("PORT", 3001)
+
+
+print(f"TestMode : {debug_mode}\nOutput Dir: {output_dir}\nPort: {port}")
+
 print("This script Works")
